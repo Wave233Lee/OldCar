d0d873152b47c370af9e2a2037cc62745da110d0
Bohower <zszszscbhcbhcbh@gmail.com>
2019-04-30 17:14:31 +0800

    推荐系统

--- src/main/java/com/example/oldcar/repository/BuyCarRepository.java
+++ src/main/java/com/example/oldcar/repository/BuyCarRepository.java
@@ -16,11 +16,10 @@ public interface BuyCarRepository extends JpaRepository<BuyCar,Long> {
     List<BuyCar> findByCar_PriceRange(Integer priceRange);
     List<BuyCar> findByCar_UseLengthRange(Integer useLengthRange);
     List<BuyCar> findByBuyerAndCar_Brand(User user, CarBrand brand);
-
-//    List<BuyCar> findByCar_Series(CarSeries series);
-//    List<BuyCar> findByCar_Level(Integer level);
-//    List<BuyCar> findByCar_PriceRange(Integer priceRange);
-//    List<BuyCar> findByCar_UseLengthRange(Integer useLengthRange);
+    List<BuyCar> findByBuyerAndCar_Series(User user, CarSeries series);
+    List<BuyCar> findByBuyerAndCar_Level(User user, Integer level);
+    List<BuyCar> findByBuyerAndCar_PriceRange(User user, Integer priceRange);
+    List<BuyCar> findByBuyerAndCar_UseLengthRange(User user, Integer useLengthRange);
     BuyCar findFirstByOrderByIdDesc();
     BuyCar findFirstByBuyerOrderByIdDesc(User user);
 }
